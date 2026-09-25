ce8964579a417805c3b53b7c372c6381e7c3e0d1
Andrew Elliza Manuhutu <2581025@unai.edu>
2026-09-25 11:14:00 +0700

    Memasukan Nilai Akhir dll

--- src/NilaiAkhir.java
+++ src/NilaiAkhir.java
@@ -17,5 +17,13 @@ double mid = input.nextDouble();
 
 System.out.print("Nilai Final : ");
 double nfinal = input.nextDouble();
+
+double akhir = praktikum*BOBOT_PRAKTIKUM + tugas*BOBOT_TUGAS + mid*BOBOT_MID + nfinal*BOBOT_FINAL;
+
+int potong = (int) akhir;
+int dibulatkan = (int) Math.round(akhir);
+double selisih = dibulatkan-akhir;
+boolean lulus = akhir >= 60;
+
     }
 }
